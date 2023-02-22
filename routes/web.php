14dbf231b228e00de70862cc8a6c9c58fa1ebc3f
pees <smorawinski.pawel@gmail.com>
2023-02-22 12:34:36 +0100

    modify routes

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,5 @@ Route::get('/', function () {
 });
 use App\Http\Controllers\UserController;
  
-// Route::resource('user', UserController::class);
+Route::resource('user', UserController::class);
 
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/users/create', [UserController::class, 'create']);
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
